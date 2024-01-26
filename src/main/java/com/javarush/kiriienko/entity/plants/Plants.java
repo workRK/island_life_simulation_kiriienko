c32a306d8ff9285fc78face3683409f2de5fa1c7
workRK <rk29work@proton.me>
2024-01-26 22:26:02 +0200

    Add plants logic

--- src/main/java/com/javarush/kiriienko/entity/plants/Plants.java
+++ src/main/java/com/javarush/kiriienko/entity/plants/Plants.java
@@ -1,4 +1,46 @@
 package com.javarush.kiriienko.entity.plants;
 
-public class Plants {
+import com.javarush.kiriienko.abstraction.Eatable;
+import com.javarush.kiriienko.entity.map.Cell;
+
+public class Plants implements Eatable, Runnable {
+
+    private final double maxCellCapacity;
+    private final double growSpeed;
+    private double weight;
+    private final Cell cell;
+
+    public Plants(double maxCellCapacity, double growSpeed, double weight, Cell cell) {
+        this.maxCellCapacity = maxCellCapacity;
+        this.growSpeed = growSpeed;
+        this.weight = weight;
+        this.cell = cell;
+    }
+
+    @Override
+    public void run() {
+        grow();
+    }
+
+    @Override
+    public synchronized double satiate() {
+        return weight;
+    }
+
+    @Override
+    public synchronized void wasSatiated(double weight) {
+        this.weight = this.weight - weight;
+    }
+
+    public void grow() {
+        double maxPlantweight = ANIMAL_CONFIG.getFor("plants", "weight");
+        if (weight < maxPlantweight) {
+            weight = weight + maxPlantweight + growSpeed;
+        }
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
+
