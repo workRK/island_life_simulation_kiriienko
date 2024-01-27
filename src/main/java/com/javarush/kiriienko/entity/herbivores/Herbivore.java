4f3523e829079dcff4f3ee685acbdb7278a67eb1
workRK <rk29work@proton.me>
2024-01-28 00:05:43 +0200

    Create models Predators

--- src/main/java/com/javarush/kiriienko/entity/herbivores/Herbivore.java
+++ src/main/java/com/javarush/kiriienko/entity/herbivores/Herbivore.java
@@ -1,6 +1,11 @@
 package com.javarush.kiriienko.entity.herbivores;
 
+import com.javarush.kiriienko.animal_factory.Gender;
 import com.javarush.kiriienko.entity.Animal;
+import com.javarush.kiriienko.entity.map.Cell;
 
 public abstract class Herbivore extends Animal {
+    public Herbivore(double weight, int maxCellPopulation, int speed, double maxSaturation, Cell cell, int offspring, Gender gender, double saturation, int daysBeforeDecay) {
+        super(weight, maxCellPopulation, speed, maxSaturation, cell, offspring, gender, saturation, daysBeforeDecay);
+    }
 }
--- src/main/java/com/javarush/kiriienko/entity/predators/Predator.java
+++ src/main/java/com/javarush/kiriienko/entity/predators/Predator.java
@@ -1,6 +1,11 @@
 package com.javarush.kiriienko.entity.predators;
 
+import com.javarush.kiriienko.animal_factory.Gender;
 import com.javarush.kiriienko.entity.Animal;
+import com.javarush.kiriienko.entity.map.Cell;
 
 public abstract class Predator extends Animal {
+    public Predator(double weight, int maxCellPopulation, int speed, double maxSaturation, Cell cell, int offspring, Gender gender, double saturation, int daysBeforeDecay) {
+        super(weight, maxCellPopulation, speed, maxSaturation, cell, offspring, gender, saturation, daysBeforeDecay);
+    }
 }
